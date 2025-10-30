4066be6c87be510494ba6262715685691fb7a013
Zantech12 <dontechiel@gmail.com>
2025-10-30 04:50:58 +0100

    hhhh

--- server.js
+++ server.js
@@ -61,6 +61,6 @@ mongoose
     .catch((err) => console.error("❌ MongoDB Connection Error:", err.message));
 
 // --- Start Server ---
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
