56a726d5a9ab9e921be4619edd12d89fe85ae816
Zantech12 <dontechiel@gmail.com>
2025-12-03 08:14:05 +0100

    sxsxs

--- server.js
+++ server.js
@@ -54,8 +54,6 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/signupsch"
 
 mongoose
     .connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
     })
     .then(() => console.log("✅ MongoDB Connected Successfully"))
     .catch((err) => console.error("❌ MongoDB Connection Error:", err.message));
